64b762c2686a9d57a56a2965d1ea85bb4bfc5959
Amory Galili <amorygalili@gmail.com>
2021-04-12 22:26:54 -0400

    Fixed errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import '@tensorflow/tfjs';
 
+// Note: Require the cpu and webgl backend and add them to package.json as peer dependencies.
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
+
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+
+(async () => {
+  const img = document.querySelector('img');
+
+  // Load the model.
+  console.log('Loading model...');
+  const model = await cocoSsd.load();
+  console.log('Model loaded');
+
+  // Classify the image.
+  console.log('Classifying image...');
+  const predictions = await model.detect(img);
+  console.log('Image classified');
+
+  console.log('Predictions: ');
+  console.log(predictions);
+})();
