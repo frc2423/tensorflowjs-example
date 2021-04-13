d696fdb1b76c5b940c3db6424dba160e8af597f7
Amory Galili <amorygalili@gmail.com>
2021-04-12 21:51:56 -0400

    Setup webpack, package.json, and added dependencies

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: './index.js',
+  output: {
+    filename: "tensorflowjs-example.js",
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [{
+            loader: 'babel-loader'
+        }]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ],
+      },
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build/'),
+    stats: 'errors-only',
+    open: true,
+    port: 12000,
+  },
+}
